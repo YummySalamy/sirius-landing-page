7b0829f2e6e81efcec147f1af5215a314ad31767
Sebastian <carsebastian1013@gmail.com>
2024-09-14 22:47:25 -0500

    version (0.0.0.2)

--- src/components/hero/BasicHero.jsx
+++ src/components/hero/BasicHero.jsx
@@ -8,11 +8,18 @@ const BasicHero = () => {
             <div className="hero-content">
                 <h1 className="hero-title">Welcome to <span>Sirius</span></h1>
                 <p className="hero-text">Bringing the best education to your doorstep</p>
-                <Link to="/services" className="hero-cta">
-                    <button className="cta-btn">
-                        Learn More
-                    </button>
-                </Link>
+                <div className="hero-buttons">
+                    <Link to="/services" className="hero-cta">
+                        <button className="cta-btn">
+                            Learn More
+                        </button>
+                    </Link>
+                    <Link to="/contact" className="hero-cta">
+                        <button className="cta-btn-1">
+                            Contact Us
+                        </button>
+                    </Link>
+                </div>
             </div>
         </section>
     )
